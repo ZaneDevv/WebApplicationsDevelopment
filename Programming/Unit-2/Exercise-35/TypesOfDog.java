9330aac894fb0563abefcbb2ff36df42e79eff53
Álvaro Fernández <alvferbarrero@gmail.com>
2025-10-23 10:02:55 +0200

    Completed exercise

--- Programming/Unit-2/Exercise-35/TypesOfDog.java
+++ Programming/Unit-2/Exercise-35/TypesOfDog.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class TypesOfDog
 {
-	private static enum DogType
+	private enum DogType
 	{
 		Mastiff,
 		Terrier,
@@ -18,11 +18,16 @@ public class TypesOfDog
 		// Defining variables
 		
 		DogType dog1 = DogType.Mastiff;
-		DogType dog2 = DogType.Bulldog;
+		DogType dog2 = DogType.Poodle;
+		
+		int ordinal1 = dog1.ordinal();
+		int ordinal2 = dog2.ordinal();
 		
 		
 		// Printing data
 		
+		System.out.printf("Position dog 1: %d\n", ordinal1);
+		System.out.printf("Position dog 2: %d\n", ordinal2);
 		System.out.print(dog1 == dog2 ? "They\'re the same dog." : "They\'re different types of dog.");
 	}
 }
