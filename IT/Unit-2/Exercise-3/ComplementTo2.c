64ed7a08c5e076b128eea3eb268a1c13cfb804dd
Álvaro Fernández <alvferbarrero@gmail.com>
2025-11-06 10:08:22 +0100

    Bitwise operations

---
+++ IT/Unit-2/Exercise-3/ComplementTo2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * @date 2025/11/06
+ * @author Álvaro Fernández Barrero
+ */
+
+
+
+/**
+ * @brief Cmputes the negative version of the given number (-x)
+ * @param The number to turn negative
+ * @return The number's negative version
+ * @version 1.0
+ * @date 2025/11/06
+ * @author Álvaro Fernández Barrero
+ */
+int negativeNumber(int);
+
+
+
+int main()
+{
+	int x = 0;
+
+	printf("Give me a number: ");
+	scanf("%d", &x);
+
+	printf("%d x (-1) = %d", x, negativeNumber(x));
+
+	return 0;
+}
+
+
+
+int negativeNumber(int x)
+{
+	return ~x + 1;
+}
---
+++ IT/Unit-2/Exercise-4/LogicGates.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * @date 2025/11/06
+ * @author Álvaro Fernández Barrero
+ */
+
+int main()
+{
+	// Defining variables
+
+	int a = 0;
+	int b = 0;
+
+
+	// Setting the values
+
+	printf("Set the value of a [0/1]: ");
+	scanf("%d", &a);
+
+	printf("Set the value of b [0/1]: ");
+	scanf("%d", &b);
+
+	a = a < 0 ? 0 : a > 1 : 1;
+	b = b < 0 ? 0 : b > 1 : 1;
+
+
+	// Printing result
+
+	printf("%d AND %d = %d\n", a, b, a & b);
+	printf("%d OR %d = %d\n", a, b, a | b);
+	printf("%d XOR %d = %d\n", a, b, a ^ b);
+
+
+	return 0;
+}
