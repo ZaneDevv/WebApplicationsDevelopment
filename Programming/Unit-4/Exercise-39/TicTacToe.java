b3d179c35fab47747eb63bc781ddd7dc87de0c76
Álvaro Fernández <alvferbarrero@gmail.com>
2025-11-15 22:09:26 +0100

    Better Tic-Tac-Toe game

--- Programming/Unit-4/Exercise-39/TicTacToe.java
+++ Programming/Unit-4/Exercise-39/TicTacToe.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * @version 3.0
+ * @version 3.1
  * @date 2025/11/14
  * @author Álvaro Fernández Barrero
  */
@@ -15,18 +15,15 @@ public class TicTacToe
 	private static final String ANSI_CYAN = "\u001B[36m";
 
 
-
 	// Settings
 
 	private static char PLAYER_X = 'X';
 	private static char PLAYER_O = 'O';
 
 
-
 	// Attributes
 
 	private static char[][] board = new char[3][3];
-
 
 
 
@@ -36,35 +33,44 @@ public class TicTacToe
 
 		Scanner input = new Scanner(System.in);
 
-
-		boolean hasXWin = false;
-		boolean hasOWin = false;
-
+		boolean hasXWon = false;
+		boolean hasOWon = false;
 
 
 		// Starting game
 
-		while (!hasXWin && !hasOWin)
+		printBoard();
+		System.out.print("\n");
+
+		while (!hasXWon && !hasOWon && !isBoardFull())
 		{
-			hasXWin = playerPlay(PLAYER_X, input);
+			hasXWon = playerPlay(PLAYER_X, input);
 			printBoard();
 
-			if (!hasXWin)
+
+			if (!hasXWon && !isBoardFull())
 			{
-				hasOWin = playerPlay(PLAYER_O, input);
+				hasOWon = playerPlay(PLAYER_O, input);
 				printBoard();
 			}
 		}
 
 
-		if (hasXWin)
+		// Finishing the game
+
+		if (hasXWon)
 		{
-			System.out.print("\n\nX has win!!!");
+			System.out.print("\n\nX has won!!!");
 		}
 
-		if (hasOWin)
+		if (hasOWon)
 		{
-			System.out.print("\n\nX has win!!!");
+			System.out.print("\n\nO has won!!!");
+		}
+
+		if (!hasXWon && !hasOWon && isBoardFull())
+		{
+			System.out.print("\n\nTie!!!");
 		}
 
 
@@ -172,17 +178,7 @@ public class TicTacToe
 	 */
 	private static boolean hasPlayerWon(char player, int lastX, int lastY)
 	{
-		if (checkDiagonals(player))
-		{
-			return true;
-		}
-
-		return (board[0][0] == player && board[1][0] == player && board[2][0] == player)
-			|| (board[0][0] == player && board[0][1] == player && board[0][2] == player)
-			|| (board[0][1] == player && board[1][1] == player && board[2][1] == player)
-			|| (board[1][0] == player && board[1][1] == player && board[1][2] == player)
-			|| (board[0][2] == player && board[1][2] == player && board[2][2] == player)
-			|| (board[2][0] == player && board[2][1] == player && board[2][2] == player);
+		return checkVertically(player) || checkHorizontally(player) || checkDiagonals(player);
 	}
 
 
@@ -211,20 +207,112 @@ public class TicTacToe
 			}
 		}
 
-		for (int i = board.length - 1; i >= 0; i++)
+		for (int i = 0; i < board.length; i++)
 		{
-			if (character != board[i][i])
+			if (character != board[board.length - i - 1][i])
 			{
 				break;
 			}
 
-			if (i == 0)
+			if (i == board.length - 1)
 			{
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+
+
+	/**
+	 * @brief Checks if the give character is in any of the vertial lines on the board
+	 * @param character The character to check
+	 * @return True if the given character is either of the vertical lines on the board, false otherwise
+	 * @since 3.1
+	 * @version 1.0
+	 * @date 2025/11/14
+	 * @author Álvaro Fernández Barrero
+	 */
+	private static boolean checkVertically(char character)
+	{
+		for (int i = 0; i < board[0].length; i++)
+		{
+			for (int j = 0; j < board.length; j++)
+			{
+				if (board[j][i] != character)
+				{
+					break;
+				}
+
+				if (j == board.length - 1)
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+
+
+	/**
+	 * @brief Checks if the give character is in any of the horizontal lines on the board
+	 * @param character The character to check
+	 * @return True if the given character is either of the horizontal lines on the board, false otherwise
+	 * @since 3.1
+	 * @version 1.0
+	 * @date 2025/11/14
+	 * @author Álvaro Fernández Barrero
+	 */
+	private static boolean checkHorizontally(char character)
+	{
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[0].length; j++)
+			{
+				if (board[i][j] != character)
+				{
+					break;
+				}
+
+				if (j == board[0].length - 1)
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+
+
+	/**
+	 * @brief Checks if the board is full and cannot play with it anymore
+	 * @return True if the board is full, false otherwise
+	 * @since 3.0
+	 * @version 1.0
+	 * @date 2025/11/15
+	 * @author Álvaro Fernández Barrero
+	 */
+	private static boolean isBoardFull()
+	{
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[0].length; j++)
+			{
+				char spot = board[i][j];
+
+				if (spot == ' ' || spot == 0)
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 
 
