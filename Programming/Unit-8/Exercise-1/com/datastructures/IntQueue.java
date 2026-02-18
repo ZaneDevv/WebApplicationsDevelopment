5ea480f02a19f4f132d007069e7f4eae070d9684
Álvaro Fernández <alvferbarrero@gmail.com>
2026-02-18 12:52:38 +0100

    Documented code

--- Programming/Unit-8/Exercise-1/com/datastructures/IntQueue.java
+++ Programming/Unit-8/Exercise-1/com/datastructures/IntQueue.java
@@ -17,8 +17,21 @@ public class IntQueue
     // Constructors
     //----------------------------------------
 
+    /**
+     * Creates a new empty queue
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public IntQueue() {}
 
+    /**
+     * Creates a new queue with all the elements in the given array
+     * @param array Array with all the ints to contain in the new queue
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public IntQueue(int[] array)
     {
         this.array = array;
@@ -28,6 +41,13 @@ public class IntQueue
     // Methods
     //----------------------------------------
 
+    /**
+     * Adds a new int to the queue. It will be the last one
+     * @param x Int to add to the queue
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public void add(int x)
     {
         int[] newArray = new int[this.array.length + 1];
@@ -41,6 +61,15 @@ public class IntQueue
         this.array = newArray;
     }
 
+    
+    /**
+     * Removes the first elemenet in the queue
+     * @return The number that was removed (1st)
+     * @throws Exception The queue is empty
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public int remove() throws Exception
     {
         if (this.isEmpty())
@@ -60,6 +89,16 @@ public class IntQueue
         return firstInt;
     }
 
+    /**
+     * Obtains the nth number of the queue
+     * @param index Number's index
+     * @return The nth number of the queue
+     * @throws Exception The current queue is empty
+     * @throws Exception The given index is out of the bounds of the queue
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public int checkNth(int index) throws Exception
     {
         if (this.array.length <= 0)
@@ -75,21 +114,51 @@ public class IntQueue
         return this.array[index];
     }
 
+    /**
+     * Obtains the first number of the queue
+     * @return The first number of the queue
+     * @throws Exception The current queue is empty
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public int checkFirst() throws Exception
     {
         return this.checkNth(0);
     }
 
+    /**
+     * Obtains the last number of the queue
+     * @return The last number of the queue
+     * @throws Exception The current queue is empty
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public int checkLast() throws Exception
     {
         return this.checkNth(this.array.length - 1);
     }
 
+    /**
+     * Checks if the given queue is empty
+     * @return True if the queue is empty, false otherwise
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public boolean isEmpty()
     {
         return this.array.length <= 0;
     }
 
+    /**
+     * Turns the current queue into a string
+     * @return The queue converted to a string
+     * @version 1.0
+     * @since 1.0
+     * @author Álvaro Fernández Barrero
+     */
     public String toString()
     {
         String toPrint = "";
