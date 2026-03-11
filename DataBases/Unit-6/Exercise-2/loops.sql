309a572f68e9568a1ef0b912e896d8c4acd66d55
Álvaro Fernández <alvferbarrero@gmail.com>
2026-03-11 09:35:30 +0100

    PL/SQL more loops for same exercises

--- DataBases/Unit-6/Exercise-2/loops.sql
+++ DataBases/Unit-6/Exercise-2/loops.sql
@@ -11,7 +11,6 @@ BEGIN
 END;
 
 
-
 -- 2. Show the numbers from 1 to 100
 
 -- Normal loop
@@ -34,7 +33,6 @@ BEGIN
         i := i + 1;
     END LOOP;
 END;
-
 
 -- For loop
 BEGIN
@@ -59,7 +57,6 @@ BEGIN
     END LOOP;
 END;
 
-
 -- While loop
 DECLARE
     i INT := 100;
@@ -69,7 +66,6 @@ BEGIN
         i := i - 1;
     END LOOP;
 END;
-
 
 -- For loop
 BEGIN
@@ -93,6 +89,7 @@ BEGIN
     END LOOP;
 END;
 
+
 -- While loop
 DECLARE
     i INT := 2;
@@ -103,6 +100,7 @@ BEGIN
     END LOOP;
 END;
 
+
 -- For loop
 BEGIN
     FOR i IN 1..100 LOOP
@@ -111,6 +109,7 @@ BEGIN
         END IF;
     END LOOP;
 END;
+
 
 
 -- 5. Show the odd numbers to from 1 to 100
@@ -146,8 +145,10 @@ BEGIN
 END;
 
 
+
 -- 6. Print the sum of all the numbers from 1 to 100
 
+-- Normal loop
 DECLARE
     i INT := 0;
     result INT := 0;
@@ -161,9 +162,35 @@ BEGIN
     dbms_output.put_line(result);
 END;
 
+-- While loop
+DECLARE
+    i INT := 1;
+    result INT := 0;
+BEGIN
+    WHILE i <= 100 LOOP
+        result := result + i;
+        i := i + 1;
+    END LOOP;
+    
+    dbms_output.put_line(result);
+END;
+
+-- For loop
+DECLARE
+    result INT := 0;
+BEGIN
+    FOR i IN 1..100 LOOP
+        result := result + i;
+    END LOOP;
+    
+    dbms_output.put_line(result);
+END;
+
+
 
 -- 7. Print the sum of all the even numbers from 1 to 100
 
+-- Normal loop
 DECLARE
     i INT := 0;
     result INT := 0;
@@ -176,6 +203,33 @@ BEGIN
     
     dbms_output.put_line(result);
 END;
+
+-- While loop
+DECLARE
+    i INT := 2;
+    result INT := 0;
+BEGIN
+    WHILE i <= 100 LOOP
+        result := result + i;
+        i := i + 2;
+    END LOOP;
+    
+    dbms_output.put_line(result);
+END;
+
+-- For loop
+DECLARE
+    result INT := 0;
+BEGIN
+    FOR i IN 1..100 LOOP
+        IF i MOD 2 = 0 THEN
+            result := result + i;
+        END IF;
+    END LOOP;
+    
+    dbms_output.put_line(result);
+END;
+
 
 
 -- 8. Print the sum of all the odd numbers from 1 to 100
@@ -193,9 +247,37 @@ BEGIN
     dbms_output.put_line(result);
 END;
 
+-- While loop
+DECLARE
+    i INT := 1;
+    result INT := 0;
+BEGIN
+    WHILE i <= 100 LOOP
+        result := result + i;
+        i := i + 2;
+    END LOOP;
+    
+    dbms_output.put_line(result);
+END;
+
+-- For loop
+DECLARE
+    result INT := 0;
+BEGIN
+    FOR i IN 1..100 LOOP
+        IF i MOD 2 = 1 THEN
+            result := result + i;
+        END IF;
+    END LOOP;
+    
+    dbms_output.put_line(result);
+END;
+
+
 
 -- 9. Print the sum of all even numbers from 1 to 100 and how many there are.
 
+-- Normal loop
 DECLARE
     totalSum INT := 0;
     numbersAmount INT := 0;
@@ -206,6 +288,84 @@ BEGIN
         numbersAmount := numbersAmount + 1;
         i := i + 2;
     EXIT WHEN i > 100;
+    END LOOP;
+    
+    dbms_output.put_line('Numbers amount: ' || TO_CHAR(numbersAmount) || ', total sum: ' || TO_CHAR(totalSum));
+END;
+
+-- While loop
+DECLARE
+    totalSum INT := 0;
+    numbersAmount INT := 0;
+    i INT := 2;
+BEGIN
+    WHILE i <= 100 LOOP
+        numbersAmount := numbersAmount + 1;
+        totalSum := totalSum + i;
+        i := i + 2;
+    END LOOP;
+
+    dbms_output.put_line('Numbers amount: ' || TO_CHAR(numbersAmount) || ', total sum: ' || TO_CHAR(totalSum));
+END;
+
+-- For loop
+DECLARE
+    totalSum INT := 0;
+    numbersAmount INT := 0;
+BEGIN
+    FOR i IN 1..100 LOOP
+        IF i MOD 2 = 0 THEN
+            totalSum := totalSum + i;
+            numbersAmount := numbersAmount + 1;
+        END IF;
+    END LOOP;
+    
+    dbms_output.put_line('Numbers amount: ' || TO_CHAR(numbersAmount) || ', total sum: ' || TO_CHAR(totalSum));
+END;
+
+
+-- 10. Print the sum of all odd numbers from 1 to 100 and how many there are.
+
+DECLARE
+    totalSum INT := 0;
+    numbersAmount INT := 0;
+    i INT := 1;
+BEGIN
+    LOOP
+        totalSum := totalSum + i;
+        numbersAmount := numbersAmount + 1;
+        i := i + 2;
+    EXIT WHEN i > 100;
+    END LOOP;
+    
+    dbms_output.put_line('Numbers amount: ' || TO_CHAR(numbersAmount) || ', total sum: ' || TO_CHAR(totalSum));
+END;
+
+-- While loop
+DECLARE
+    totalSum INT := 0;
+    numbersAmount INT := 0;
+    i INT := 1;
+BEGIN
+    WHILE i <= 100 LOOP
+        numbersAmount := numbersAmount + 1;
+        totalSum := totalSum + i;
+        i := i + 2;
+    END LOOP;
+
+    dbms_output.put_line('Numbers amount: ' || TO_CHAR(numbersAmount) || ', total sum: ' || TO_CHAR(totalSum));
+END;
+
+-- For loop
+DECLARE
+    totalSum INT := 0;
+    numbersAmount INT := 0;
+BEGIN
+    FOR i IN 1..100 LOOP
+        IF i MOD 2 = 1 THEN
+            totalSum := totalSum + i;
+            numbersAmount := numbersAmount + 1;
+        END IF;
     END LOOP;
     
     dbms_output.put_line('Numbers amount: ' || TO_CHAR(numbersAmount) || ', total sum: ' || TO_CHAR(totalSum));
