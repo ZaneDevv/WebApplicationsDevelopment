7d4efe87847b39e126c1ebde316c3caa47c3278c
ZaneDevv <alvferbarrero@gmail.com>
2026-09-21 11:58:41 +0200

    Git class 1 file gathering the whole information briefly

---
+++ learning_git.sh
@@ -0,0 +1,31 @@
+# Do not run this shell script
+exit 0;
+
+# Checking git's version
+git --version
+
+# Initialize a project in a directory:
+git init
+
+# Git's user configuration
+git config --global user.name "<git name>"
+git config --global user.email "<git email associated>"
+
+git config --list
+
+# Checking what git is registering
+git status
+git show
+
+# Adding changes to the next commit
+git add file # Wildcard . to add the whole directory
+
+# Creating a commit
+git commit -m "Message"
+
+# Checking git's logs
+git log
+
+# Restoring to the last versions of the project
+git restore file
+git reset --hard commit_id
