b3db49a97093bc4fa9eaa9e0c73be307dcea7f69
ZaneDevv <alvferbarrero@gmail.com>
2026-09-21 18:01:24 +0200

    Automated front-end project creation bash script

--- new_page.sh
+++ new_page.sh
@@ -3,14 +3,36 @@
 # Author: Álvaro Fernández Barrero
 # Date: 20-09-2026
 
-if [ $# -ne 1 ]; then
+# No parameters offered, no name set
+if [ $# -eq 0 ]; then
   echo -e "\e[1;91mYou need to write a parameter to set the new project's name\e[0m";
   exit 1;
 fi
 
-mkdir -p $1/src/styles $1/src/scripts $1/src/img $1/src/videos $1/src/audios $1/src/fonts
+# Creating the basic structure
+mkdir -p $1/src/styles $1/src/scripts
 touch $1/index.html $1/src/styles/main.css $1/src/scripts/main.js
 
+# If more than one configuration was set, manipule the 2nd argument's bits to define which directories generate
+if [ $# -ge 2 ]; then
+  if [ $(( $((2#$2)) & 1 )) -eq 1 ]; then
+    mkdir $1/src/img
+  fi
+
+  if [ $(( $((2#$2)) & 2 )) -eq 2 ]; then
+    mkdir $1/src/videos
+  fi
+
+  if [ $(( $((2#$2)) & 4 )) -eq 4 ]; then
+    mkdir $1/src/audios
+  fi
+
+  if [ $(( $((2#$2)) & 8 )) -eq 8 ]; then
+    mkdir $1/src/fonts
+  fi
+fi
+
+# Setting the default scripts for the html, css and javascript files
 cat > $1/index.html << EOF
 <!DOCTYPE html>
 
